f55ce2918b1fec783b4eadb92568e9fd0534c339
Vladimir-Maselskiy <65615017+Vladimir-Maselskiy@users.noreply.github.com>
2023-05-31 22:04:27 +0300

    feat: add no data to cart

--- src/components/CartContent/CartContent.tsx
+++ src/components/CartContent/CartContent.tsx
@@ -3,18 +3,24 @@ import React from 'react';
 import { Box } from '../Box/Box';
 import { useCartContext } from '@/context/state';
 import { CartItem } from '../CartItem/CartItem';
-import { Divider } from 'antd';
+import { Divider, Empty } from 'antd';
 import { CartPrice } from '../CartPrice/CartPrice';
 
 export const CartContent = () => {
-  const { cart, setCart } = useCartContext();
+  const { cart } = useCartContext();
   return (
     <Box>
-      {cart.map(cartItem => (
-        <CartItem key={cartItem.product._id} cartItem={cartItem} />
-      ))}
-      <Divider />
-      <CartPrice cart={cart} />
+      {cart.length > 0 ? (
+        <>
+          {cart.map(cartItem => (
+            <CartItem key={cartItem.product._id} cartItem={cartItem} />
+          ))}
+          <Divider />
+          <CartPrice cart={cart} />
+        </>
+      ) : (
+        <Empty image={Empty.PRESENTED_IMAGE_SIMPLE} />
+      )}
     </Box>
   );
 };
